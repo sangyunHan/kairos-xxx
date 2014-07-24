7d2a087043f3df6ec9da8a30bf59ff0f299935d5
sangyunHan <sangyun0628@naver.com>
2014-07-24 14:20:10 +0900

    Node js Test

--- NodeTest.js
+++ NodeTest.js
@@ -1,62 +1,43 @@
 /**
  * Created by HanSangyun on 2014-07-21.
  */
-console.log("__filename : " , __filename);
 
-process.on("exit", function(e) {
-   console.log("EXIT");
+
+var express = require("express");
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+    response.send("Hello World");
 });
 
-var errHandler1 = function(e) {
-    console.log("err1", e);
-}
-
-var errHandler2 = function(e) {
-    console.log("err2", e);
-}
-
-
-//process.on("uncaughtException", errHandler1);
-//process.on("uncaughtException", errHandler2);
-//
-//process.removeListener("uncaughtException", errHandler2);
-//
-//error
-
-
-process.on("sangyun", function() {
-   console.log("sangyun event");
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+   console.log("listening on this port : " + port);
 });
 
-process.emit("sangyun");
-
-var os = require("os");
-//console.log(os.platform());
-//console.log(os.arch());
-//console.log(os.cpus());
-
-var http = require("http");
-http.createServer(function(req, res) {
-    if(req.url == "/") {
-        res.writeHead(200, {
-            "Content-Type" : "text/html"
-        });
-        res.end("Welcome to the Chris's world");
-    } else if(req.url == "/about") {
-        res.writeHead(200, {
-            "Content-Type" : "text/html"
-        });
-        res.end("Welcome to the about page");
-    } else {
-        res.writeHead(200, {
-            "Content-Type" : "text/plain"
-        });
-        res.end("404 Error!!!!!!!!!");
-    }
 
 
-}).listen(1337, "127.0.0.1");
-
-
-console.log("server running at http://127.0.0.1:1337");
-console.log("git test");
+//var http = require("http");
+//http.createServer(function(req, res) {
+//    if(req.url == "/") {
+//        res.writeHead(200, {
+//            "Content-Type" : "text/html"
+//        });
+//        res.end("Welcome to the Chris's world");
+//    } else if(req.url == "/about") {
+//        res.writeHead(200, {
+//            "Content-Type" : "text/html"
+//        });
+//        res.end("Welcome to the about page");
+//    } else {
+//        res.writeHead(200, {
+//            "Content-Type" : "text/plain"
+//        });
+//        res.end("404 Error!!!!!!!!!");
+//    }
+//
+//
+//}).listen(1337, "127.0.0.1");
+//
+//
+//console.log("server running at http://127.0.0.1:1337");
